7dc65fe27b02848e0a187a6c04702cfa569abb39
omairazam <omairr.azam@gmail.com>
2024-11-04 12:34:23 +0500

    Initial commit

---
+++ lib/spree_netsuite_integration.rb
@@ -0,0 +1,4 @@
+require 'spree_core'
+require 'spree_extension'
+require 'spree_netsuite_integration/engine'
+require 'spree_netsuite_integration/version'
---
+++ lib/spree_netsuite_integration/version.rb
@@ -0,0 +1,11 @@
+module SpreeNetsuiteIntegration
+  VERSION = '0.0.1'.freeze
+
+  module_function
+
+  # Returns the version of the currently loaded SpreeNetsuiteIntegration as a
+  # <tt>Gem::Version</tt>.
+  def version
+    Gem::Version.new VERSION
+  end
+end
