25dfbac7a575b018d55f8e6fccd811c41ea2d0e4
umairr.azam <omairrr.azam@gmail.com>
2024-11-06 21:31:09 +0500

    add discount item

--- app/services/spree/netsuite_item_service.rb
+++ app/services/spree/netsuite_item_service.rb
@@ -1,7 +1,18 @@
 module Spree
   class NetsuiteItemService
     def self.format_item(item)
-      { item: { id: item.variant.netsuite_item_id }, rate: item.price.to_f, quantity: item.quantity }
+      { item: { id: item.variant.netsuite_item_id }, rate: rate_item(item), quantity: item.quantity }
+    end
+
+    private
+
+    def self.rate_item(item)
+      if item.variant.sale_price.present?
+        current_currency ||= Spree::Config[:currency]
+        item.variant.original_price_in(current_currency).amount.to_f
+      else
+        item.price.to_f
+      end
     end
   end
 end
