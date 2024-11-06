40f39ba4da5b3b7b8fa15a5305a4a72003c1f2bf
omairazam <omairr.azam@gmail.com>
2024-11-06 16:34:09 +0500

    fix order edit issue

--- app/controllers/spree/admin/orders_controller_decorator.rb
+++ app/controllers/spree/admin/orders_controller_decorator.rb
@@ -5,11 +5,18 @@ module Spree
       before_action :load_order, only: %i[
         create_netsuite_order
       ]
+      after_action :need_to_update_on_netsuite, only: %i[edit]
 
       after_action :update_order_on_netsuite, only: :cancel
 
       def update_order_on_netsuite
-        @order.trigger_netsuite_update_order
+        @order.trigger_netsuite_cancel_order
+      end
+
+      def need_to_update_on_netsuite
+        ActiveRecord::Base.connected_to(role: :writing) do
+          @order.update(is_updated_on_netsuite: false)
+        end
       end
 
       def create_netsuite_order
