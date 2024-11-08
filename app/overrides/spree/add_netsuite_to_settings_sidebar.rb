d668c6a1b54072da3bb0f7ff8da2e00e04b3f054
Duc T. Lam <ducl_13@yahoo.com>
2024-11-08 11:14:43 -0800

    No route registered yet

--- app/overrides/spree/add_netsuite_to_settings_sidebar.rb
+++ app/overrides/spree/add_netsuite_to_settings_sidebar.rb
@@ -1,6 +1,8 @@
+=begin
 Deface::Override.new(
   virtual_path: 'spree/admin/shared/sub_menu/_configuration',
   name: 'add_netsuite_settings_to_admin_configuration_menu',
   insert_bottom: '[data-hook="admin_configurations_sidebar_menu"]',
   text: '<%= configurations_sidebar_menu_item Spree.t(:netsuite_settings), edit_admin_netsuite_settings_path %>'
 )
+=end
