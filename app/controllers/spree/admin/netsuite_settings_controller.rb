4a22d88c26e19caef4b2e85fe270b5e1d44a68e8
umairr.azam <omairrr.azam@gmail.com>
2024-11-08 16:59:27 +0500

    feat(netsuite_setting): added netsuite setting

---
+++ app/controllers/spree/admin/netsuite_settings_controller.rb
@@ -0,0 +1,35 @@
+module Spree
+  class Admin::NetsuiteSettingsController < Spree::Admin::BaseController
+    before_action :set_netsuite_setting
+
+    def edit
+    end
+
+    def update
+      if @netsuite_setting.update(netsuite_settings_params.merge(active: netsuite_settings_params[:active] == '1'))
+        flash[:success] = "Setting updated"
+        redirect_to edit_admin_netsuite_settings_path
+      else
+        flash[:error] = "Setting Failed"
+        render :edit
+      end
+    end
+
+    private
+
+    def set_netsuite_setting
+      @netsuite_setting = Spree::NetsuiteSetting.first_or_initialize
+    end
+
+
+    def netsuite_settings_params
+      params.require(:netsuite_setting).permit(
+        :netsuite_entity_id,
+        :netsuite_location_id,
+        :netsuite_check_payment_method_id,
+        :netsuite_online_payment_method_id,
+        :active
+      )
+    end
+  end
+end
