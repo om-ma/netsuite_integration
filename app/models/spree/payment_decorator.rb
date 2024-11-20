c03ca395010591120b604dddfc856f8ad28f1548
umairr.azam <omairrr.azam@gmail.com>
2024-11-20 17:16:18 +0500

    updated code

--- app/models/spree/payment_decorator.rb
+++ app/models/spree/payment_decorator.rb
@@ -2,7 +2,7 @@ module Spree
   module PaymentDecorator
 
     def trigger_netsuite_update
-      if defined?(Spree::NetsuiteSetting) && Spree::NetsuiteSetting.respond_to?(:active?) && Spree::NetsuiteSetting.active?
+      if Spree.constants.include?(:NetsuiteSetting) && Spree::NetsuiteSetting.respond_to?(:active?) && Spree::NetsuiteSetting.active?
         NetsuiteUpdatePaymentWorker.perform_async(self.order.id)
       end
     end
