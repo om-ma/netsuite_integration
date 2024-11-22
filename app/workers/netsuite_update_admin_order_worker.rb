fec7bd9aafad9fd79d27efe9df1ef5bec23cd8be
umairrazam <omairrr.azam@gmail.com>
2024-11-22 16:52:32 +0500

    fix order update issues

--- app/workers/netsuite_update_admin_order_worker.rb
+++ app/workers/netsuite_update_admin_order_worker.rb
@@ -1,9 +1,8 @@
 class NetsuiteUpdateAdminOrderWorker
   include Sidekiq::Worker
 
-  def perform(order_id)
-    order = Spree::Order.find(order_id)
-      Spree::NetsuiteUpdateLineItemService.new.update(order)
+  def perform(order_id, line_item_ids)
+    Spree::NetsuiteUpdateLineItemService.new.update(order_id, line_item_ids)
   rescue => e
     Rails.logger.error("Failed to payment on NetSuite: #{e.message}")
   end
