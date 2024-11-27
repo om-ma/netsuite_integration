48ab35a6602618c229f6b5806149634572b301ee
umairrazam <omairrr.azam@gmail.com>
2024-11-27 20:35:51 +0500

    fix spree version

--- app/overrides/spree/add_netsuite_order_to_admin_order_summary.rb
+++ app/overrides/spree/add_netsuite_order_to_admin_order_summary.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   virtual_path: 'spree/admin/shared/_order_summary',
-  name: 'add_netsuite_order_to_admin_order_summary',
-  insert_before: "[data-hook='admin_order_tab_date_completed_title']",
-  partial: "spree/admin/orders/netsuite_sales_order_num"
+  name: 'add_netsuite_order_to_admin_order_summary_table',
+  insert_after: 'table#order_tab_summary > tbody.additional-info',
+  partial: 'spree/admin/orders/netsuite_sales_order_num'
 )
--- spree_netsuite_integration.gemspec
+++ spree_netsuite_integration.gemspec
@@ -21,7 +21,7 @@ Gem::Specification.new do |s|
   s.require_path = 'lib'
   s.requirements << 'none'
 
-  s.add_dependency 'spree', '>= 3.7', '<= 4.5'
+  s.add_dependency 'spree', '>= 3.7', '<= 4.7'
   # s.add_dependency 'spree_backend' # uncomment to include Admin Panel changes
   s.add_dependency 'spree_extension'
 
